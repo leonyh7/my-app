205af66cefccb930569ee8128e3b6b8829fc786b
荒原N旅人 <leo_nyh@sina.com>
2017-09-28 11:58:41 +0800

    init

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+    selector: 'hero-detail',
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+    hero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) { }
+
+    ngOnInit(): void {
+        this.route.paramMap
+            .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+            .subscribe(hero => this.hero = hero);
+    }
+
+    /**
+     * 返回上一页
+     */
+    goBack(): void {
+        this.location.back();
+    }
+
+    /**
+     * 保存修改
+     */
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
+}
